77170a8424285af892ccc845601e419a028111ec
PirateKaiju <santosgonsalvesjr@gmail.com>
2020-03-27 00:49:38 -0300

    First Commit

---
+++ app/Http/Controllers/ExampleController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Auth;
+use Illuminate\Http\Request;
+
+class ExampleController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+        $this->middleware('auth');
+    }
+
+    public function testlogin(Request $request){
+
+        if(Auth::user()->name == "testman"){
+
+            return "Ok";
+
+        }
+
+        return "Unavailable";
+        
+
+    }
+
+    //
+}
